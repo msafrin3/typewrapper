d24d199d799af74d4f789984669760a4129228a9
Safrin <safrin.carter94@gmail.com>
2023-06-16 23:42:29 +0800

    migrations

---
+++ database/migrations/2023_06_16_225103_create_meta_datas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('meta_datas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('meta_id')->constrained('meta_datas');
+            $table->string('name');
+            $table->string('group_helper')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('meta_datas');
+    }
+};
